2a6cc053af9d5e1ab6600235955ed0f9e2fec1c2
Jaeyo <jaeyo.developer@gmail.com>
2019-06-18 22:38:56 +0900

    styled-components adopted to sidebar

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,56 +1,32 @@
+import { lighten } from 'polished'
 
 
-export const lightenColor = (color, percent) => {
-    const num = parseInt(color.replace('#', ''), 16)
-    const amt = Math.round(2.55 * percent)
-    const R = (num >> 16) + amt
-    const B = (num >> 8 & 0x00FF) + amt
-    const G = (num & 0x0000FF) + amt
+export const colorBase = '#222222'
+export const colorPrimary = '#5d93ff'
+export const colorSecondary = '#f7a046'
+export const colorWhite = lighten(1, colorBase)
+export const colorGray = lighten(0.4, colorBase)
+export const colorGrayBorder = lighten(0.77, colorBase)
+export const colorGrayBg = lighten(0.79, colorBase)
 
-    return (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (B<255?B<1?0:B:255)*0x100 + (G<255?G<1?0:G:255)).toString(16).slice(1);
-}
-
-export const darkenColor = (color, percent) => lightenColor(color, -percent)
-
-
-
-const base = '#222222'
-const primary = '#5d93ff'
-const secondary = '#f7a046'
-
-export const color = {
-    base,
-    primary,
-    secondary,
-    white: lightenColor(base, 100),
-    gray: lightenColor(base, 40),
-    grayBorder: lightenColor(base, 77),
-    grayBg: lightenColor(base, 79),
-}
 
 export const typographicFontFamily = '"Roboto", "Helvetica Neue", "Helvetica", "Arial", sans-serif'
 
-export const typographic = {
-    FontFamily: '"Roboto", "Helvetica Neue", "Helvetica", "Arial", sans-serif',
-    rootFontSize: 100,
-    baseFontSize: '16px',
-    smallFontSize: '14px',
-    baseLineHeight: 1.625,
-    baseFontColor: color.base,
-    linkPFontColor: color.primary,
-    linkSFontColor: color.secondary,
-}
+export const typographicRootFontSize = 100
+export const typographicBaseFontSize = '16px'
+export const typographicSmallFontSize = '14px'
+export const typographicBaseLineHeight = 1.625
+export const typographicBaseFontColor = colorBase
+export const typographicLinkPFontColor = colorPrimary
+export const typographicLinkSFontColor = colorSecondary
 
+export const typographicLeading =
+    Math.round(16 * (typographicRootFontSize / 100) * typographicBaseLineHeight)
 
-// TODO IMME
+export const layoutPostSingleMaxWidth = '945px'
+export const layoutPostSingleWidth = `${layoutPostSingleMaxWidth} - 305px`
 
-$typographic-leading: round(16 * ($typographic-root-font-size / 100) * $typographic-base-line-height);
-
-// Layout
-$layout-post-single-max-width: 945px;
-$layout-post-single-width: $layout-post-single-max-width - 305px;
-
-$layout-width: 1070px;
-$layout-breakpoint-sm: 685px;
-$layout-breakpoint-md: 960px;
-$layout-breakpoint-lg: 1100px;
+export const layoutWidth = '1070px'
+export const layoutBreakpointSm = '685px'
+export const layoutBreakpointMd = '960px'
+export const layoutBreakpointLg = '1100px'
