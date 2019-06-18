bf190f75d2527a6f22c31fe16a2ab499f57c7ccf
Jaeyo <jaeyo.developer@gmail.com>
2019-06-18 23:07:03 +0900

    styled-components adopted to post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import { Link } from 'gatsby'
 import moment from 'moment'
-import './style.scss'
+import styled from 'styled-components'
+import { typographicLeading, typographicBaseFontSize, colorBase, typographicSmallFontSize, colorSecondary, colorPrimary } from '../../styles/variables'
 
-class Post extends Component {
+export default class Post extends Component {
   render() {
     const {
       title,
@@ -14,33 +15,90 @@ class Post extends Component {
     const { slug, categorySlug } = this.props.data.node.fields
 
     return (
-      <div className="post">
-        <div className="post__meta">
-          <time
-            className="post__meta-time"
-            dateTime={moment(date).format('MMMM D, YYYY')}
-          >
+      <PostWrapper>
+        <Meta>
+          <MetaTime dateTime={moment(date).format('MMMM D, YYYY')}>
             {moment(date).format('MMMM YYYY')}
-          </time>
-          <span className="post__meta-divider" />
-          <span className="post__meta-category" key={categorySlug}>
-            <Link to={categorySlug} className="post__meta-category-link">
+          </MetaTime>
+          <MetaDivider />
+          <MetaCategory key={categorySlug}>
+            <MetaCategoryLink to={categorySlug}>
               {category}
-            </Link>
-          </span>
-        </div>
-        <h2 className="post__title">
-          <Link className="post__title-link" to={slug}>
+            </MetaCategoryLink>
+          </MetaCategory>
+        </Meta>
+        <Title>
+          <TitleLink to={slug}>
             {title}
-          </Link>
-        </h2>
-        <p className="post__description">{description}</p>
-        <Link className="post__readmore" to={slug}>
+          </TitleLink>
+        </Title>
+        <Description>{description}</Description>
+        <Readmore to={slug}>
           Read
-        </Link>
-      </div>
+        </Readmore>
+      </PostWrapper>
     )
   }
 }
 
-export default Post
+const PostWrapper = styled.div`
+  margin-bottom: ${1.25 * typographicLeading}px;
+  &:last-child {
+    margin-bottom: ${0.5 * typographicLeading}px;
+  }
+`
+
+const Title = styled.h2`
+  font-size: ${typographicBaseFontSize * 1.6875}px;
+  line-height: ${1.5 * typographicLeading}px;
+  margin-top: ${0 * typographicLeading}px;
+  margin-bottom: ${0.5 * typographicLeading}px;
+`
+
+const TitleLink = styled(Link)`
+  color: ${colorBase};
+  &:hover, &:focus {
+    color: ${colorBase};
+    border-bottom: 1px solid ${colorBase};
+  }
+`
+
+const Description = styled.p`
+  font-size: ${typographicBaseFontSize}px;
+  line-height: ${1 * typographicLeading}px;
+  margin-bottom: ${0.75 * typographicLeading}px;
+`
+
+const Meta = styled.div``
+
+const MetaTime = styled.time`
+  font-size: ${typographicSmallFontSize}px;
+  color: ${colorBase};
+  font-weight: 500;
+  text-transform: uppercase;
+`
+
+const MetaDivider = styled.span`
+  margin: 0 5px;
+`
+
+const MetaCategory = styled.span``
+
+const MetaCategoryLink = styled(Link)`
+  font-size: ${typographicSmallFontSize}px;
+  color: ${colorSecondary};
+  font-weight: 500;
+  text-transform: uppercase;
+  &:hover, &:focus {
+    color: ${colorPrimary};
+  }
+`
+
+const Readmore = styled(Link)`
+  font-size: ${typographicBaseFontSize}px;
+  color: ${colorPrimary};
+  &:hover, &:focus {
+    color: ${colorPrimary};
+    border-bottom: 1px solid ${colorPrimary};
+  }
+`
