f1a9ba3c48e544553d8bffed4035361504084ff4
이재용 <jaeyo.developer@gmail.com>
2019-06-19 18:46:57 +0900

    apply styled-components to menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,29 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
-import './style.scss'
+import styled from 'styled-components'
+import { typographicLeading, typographicBaseFontSize, typographicBaseFontColor, colorPrimary, colorBase } from '../../styles/variables'
 
-class Menu extends React.Component {
+
+export default class Menu extends Component {
   render() {
     const menu = this.props.data
 
-    const menuBlock = (
-      <ul className="menu__list">
-        {menu.map(item => (
-          <li className="menu__list-item" key={item.path}>
-            <Link
-              to={item.path}
-              className="menu__list-item-link"
-              activeClassName="menu__list-item-link menu__list-item-link--active"
-            >
-              {item.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    return (
+      <MenuWrapper>
+        <MenuList>
+          {menu.map(item => (
+            <MenuListItem key={item.path}>
+              <MenuListItemLink
+                to={item.path}
+                activeStyle={{
+                  color: colorBase,
+                  borderBottom: `1px solid ${colorBase}`,
+                }}
+              >
+                {item.label}
+              </MenuListItemLink>
+            </MenuListItem>
+          ))}
+        </MenuList>
+      </MenuWrapper>
     )
-
-    return <nav className="menu">{menuBlock}</nav>
   }
 }
 
-export default Menu
+const MenuWrapper = styled.nav`
+  margin-bottom: ${1 * typographicLeading}px;
+`
+
+const MenuList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`
+
+const MenuListItem = styled.li`
+  padding: 0;
+  margin: 10px 0;
+`
+
+const MenuListItemLink = styled(Link)`
+  font-size: ${typographicBaseFontSize}px;
+  color: ${typographicBaseFontColor};
+  font-weight: 400;
+  border: 0;
+  &:hover, &:focus {
+    color: ${colorPrimary};
+    border-bottom: 1px solid ${colorPrimary};
+  }
+`
