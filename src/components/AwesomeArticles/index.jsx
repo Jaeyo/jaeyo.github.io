c9a5304283ce5f3a1b4706488776f3cabb84306a
이재용 <jaeyo.developer@gmail.com>
2019-06-21 15:48:28 +0900

    -

---
+++ src/components/AwesomeArticles/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+
+export default class AwesomeArticle extends Component {
+  render() {
+    return (
+      <div>hello, awesome article</div>
+    )
+  }
+}
