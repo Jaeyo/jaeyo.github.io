e8cfd1e75d3e6a1d870106851e6c7d4210a4f2a8
이재용 <jaeyo.developer@gmail.com>
2019-08-13 15:21:36 +0900

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-142557602-1"
       },
     },
     `gatsby-plugin-feed`,
